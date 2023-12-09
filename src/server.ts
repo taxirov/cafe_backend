5bf3676dcdff609a0f3ae0fde506f712a57aae7a
Saad Takhir <saadtakhir1@gmail.com>
2023-12-09 17:26:04 +0500

    fixing cors

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,9 @@ createRoleAdmin();
 const app = express();
 const port = +process.env.PORT! || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:8080", "http://31.129.110.72:8080"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
