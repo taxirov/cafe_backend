36462484e01af9ec80f60dd04379de712453f380
Saad Takhir <saadtakhir1@gmail.com>
2023-12-09 21:48:11 +0500

    update category

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -18,7 +18,16 @@ export class CategoryController {
           message: "Category already exsist"
         })
       } else {
-        const category = await categoryService.create({ name, desc });
+        const category_created = await categoryService.create({ name, desc });
+        let category: CategoryResponse = {
+          id: category_created.id,
+          name: category_created.name,
+          desc: category_created.desc,
+          image: category_created.image,
+          products: await productService.findByCategoryId(category_created.id),
+          create_date: category_created.create_date,
+          update_date: category_created.update_date
+        }
         res.status(201).json({
           message: "Category created",
           category
@@ -62,9 +71,18 @@ export class CategoryController {
           message: "Not found category with id: " + id
         })
       } else {
-        const category = await categoryService.delete(+id)
+        const category_deleted = await categoryService.delete(+id)
+        let category: CategoryResponse = {
+          id: category_deleted.id,
+          name: category_deleted.name,
+          desc: category_deleted.desc,
+          image: category_deleted.image,
+          products: await productService.findByCategoryId(category_deleted.id),
+          create_date: category_deleted.create_date,
+          update_date: category_deleted.update_date
+        }
         res.status(200).json({
-          message: category.name + " category deleted",
+          message: "Category success deleted",
           category
         })
       }
@@ -84,7 +102,16 @@ export class CategoryController {
           message: "Category not found by id: " + id
         })
       } else {
-        const category = await categoryService.update(+id, name, desc)
+        const category_updated = await categoryService.update(+id, name, desc)
+        let category: CategoryResponse = {
+          id: category_updated.id,
+          name: category_updated.name,
+          desc: category_updated.desc,
+          image: category_updated.image,
+          products: await productService.findByCategoryId(category_updated.id),
+          create_date: category_updated.create_date,
+          update_date: category_updated.update_date
+        }
         res.status(200).json({
           message: "Category success updated",
           category
