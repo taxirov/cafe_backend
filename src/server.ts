d386c90f2a963f1f0389a786454ea55b6aca685d
Saad Takhir <saadtakhir1@gmail.com>
2023-12-15 10:58:55 +0500

    add  header

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,13 @@ createRoleAdminWaiter();
 const app = express();
 const port = +process.env.PORT! || 3000;
 
-app.use(cors({ origin: '*', methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}));
+app.use(cors());
+// allow CORS:
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://madatota.vercel.app/');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
