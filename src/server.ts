66bdf9fa685415128b389971e85f7c641dc595cc
Saad Takhir <saadtakhir1@gmail.com>
2023-12-24 01:01:41 +0500

    add header

--- src/server.ts
+++ src/server.ts
@@ -22,10 +22,7 @@ const credentials = {key: privateKey, cert: certificate};
 
 const app = express();
 
-app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
