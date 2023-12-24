7fa77b67b37d2bf307439ddaa9a2f2812d6b6fa2
Saad Takhir <saadtakhir1@gmail.com>
2023-12-24 11:24:34 +0500

    add cors

--- src/server.ts
+++ src/server.ts
@@ -23,11 +23,16 @@ const credentials = {key: privateKey, cert: certificate};
 const app = express();
 
 app.use(cors({
-  origin: 'https://madatota.vercel.app', 
+  origin: '*', 
   credentials: true,
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
+
+app.use((req, res, next) => {
+  console.log('Request received:', req.method, req.url);
+  next();
+});
 
 const port = +process.env.PORT! || 3000;
 
